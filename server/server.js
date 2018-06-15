42d020df7df371c197170a42f0b314216164a910
Ant Kaynak <antkaynak1@gmail.com>
2018-06-15 17:47:02 +0300

    fixed not found issue

--- server/server.js
+++ server/server.js
@@ -23,13 +23,14 @@ app.get('/todos', (req,res)=>{
 
 app.get('/todos/:id', (req,res)=>{
     console.log('GET /todos/:id ', req.params);
-    if(req.params == null || req.params.length == 0){
-        return res.status(400).send('Id is required');
-    }
-    if(req.params.id == null || !ObjectID.isValid(req.params.id)){
+    const id = req.params.id;
+    if(!ObjectID.isValid(id)){
         return res.status(404).send('Id is not valid');
     }
-    Todo.findById(req.params.id).then((todo)=>{
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(400).send('Todo with the id not found ' + id);
+        }
         res.status(200).send(todo);
     }, (err)=>{
         console.log('An error occurred fetching user with id'+ req.params.id+ ' \n', err);
